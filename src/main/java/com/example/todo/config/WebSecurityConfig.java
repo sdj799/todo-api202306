4974b4b7522148a0f1b09936d8d7931ebdfc8246
dongjun <sdj799@naver.com>
2023-06-26 13:13:45 +0900

    # 5.토큰 인증 처리

--- src/main/java/com/example/todo/config/WebSecurityConfig.java
+++ src/main/java/com/example/todo/config/WebSecurityConfig.java
@@ -1,16 +1,25 @@
 package com.example.todo.config;
 
+import com.example.todo.filter.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
 
 //@Configuration //설정 클래스 용도로 사용하도록 스프링에 등록하는 아노테이션
+@RequiredArgsConstructor
 @EnableWebSecurity //시큐리티 설정 파일로 사용할 클래스 선언.
 public class WebSecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -24,7 +33,22 @@ public class WebSecurityConfig {
                 .cors()
                 .and()
                 .csrf().disable()
-                .httpBasic().disable();
+                .httpBasic().disable()
+                //세션 인증을 사용하지 않겠다
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                //어떤 요청에서 인증을 안 할 것인지 설정, 언제 할 것인지 설정
+                .authorizeRequests()
+                .antMatchers("/"/*localhost:8181*/,"/api/auth/**").permitAll()
+//                .antMatchers(HttpMethod.POST, "/api/todos").hasRole("ADMIN")
+                .anyRequest().authenticated();
+        //토큰 인증 필터 연결
+        http.addFilterAfter(
+                jwtAuthFilter,
+                CorsFilter.class // import 주의: 스프링 꺼로
+        );
+
         return http.build();
     }
 }
